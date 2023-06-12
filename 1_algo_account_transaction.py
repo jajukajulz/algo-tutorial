6335fd5c926bfee8d672025ff70d3e9ac1d1797a
Julian Kanjere <julian.kanjere@jskdezign.com>
2023-06-12 23:47:33 +0300

    add python scripts for algo sdk

---
+++ 1_algo_account_transaction.py
@@ -0,0 +1,58 @@
+# Description - Interact with Algorand blockchain using the Python SDK py-algorand-sdk
+# Before running, make sure you have installed py-algorand-sdk i.e. pip3 install py-algorand-sdk
+# Usage - $ python 1_algo_account_transaction.py
+
+# from utils import get_accounts, get_algod_client
+import json
+
+from typing import Dict, Any
+from base64 import b64decode
+
+from algosdk import account, mnemonic
+from algosdk import transaction
+from algosdk.v2client import algod
+
+
+MY_PERA_ADDRESS = "GRZANBMEE3JDUGNIMR5ZGLRHJ3N3OQFJAAHNFLQZI4GJMBZYEYWKNTT3GQ"
+
+# example: ACCOUNT_RECOVER_MNEMONIC
+sender_mnemonic = "cost piano sample enough south bar diet garden nasty mystery mesh sadness convince bacon best patch surround protect drum actress entire vacuum begin abandon hair"
+sender_pk = mnemonic.to_private_key(sender_mnemonic)
+print(f"Base64 encoded private key: {sender_pk}")
+sender_addr = account.address_from_private_key(sender_pk)
+print(f"Address: {sender_addr}")
+# example: ACCOUNT_RECOVER_MNEMONIC
+
+
+# Create a new algod client, configured to connect to your local sandbox or a node e.g. https://algonode.io/
+algod_address = "https://testnet-api.algonode.cloud"
+algod_token = ""
+
+algod_client = algod.AlgodClient(algod_token, algod_address)
+
+account_info: Dict[str, Any] = algod_client.account_info(sender_addr)
+print(f"Account balance: {account_info.get('amount')} microAlgos")
+
+# grab suggested params from algod using client
+# includes things like suggested fee and first/last valid rounds
+params = algod_client.suggested_params()
+unsigned_txn = transaction.PaymentTxn(
+    sender=sender_addr,
+    sp=params,
+    receiver=MY_PERA_ADDRESS,
+    amt=1000000,
+    note=b"Hello World",
+)
+
+# sign the transaction
+signed_txn = unsigned_txn.sign(sender_pk)
+
+# submit the transaction and get back a transaction id
+txid = algod_client.send_transaction(signed_txn)
+print("Successfully submitted transaction with txID: {}".format(txid))
+
+# wait for confirmation
+txn_result = transaction.wait_for_confirmation(algod_client, txid, 4)
+
+print(f"Transaction information: {json.dumps(txn_result, indent=4)}")
+print(f"Decoded note: {b64decode(txn_result['txn']['txn']['note'])}")
---
+++ 2_algo_multisig_rekey.py
@@ -0,0 +1,110 @@
+# Description - Interact with Algorand blockchain using the Python SDK py-algorand-sdk
+# Before running, make sure you have installed py-algorand-sdk i.e. pip3 install py-algorand-sdk
+# Usage - $ python 2_algo_multisig_rekey.py
+
+import json, time
+
+from typing import Dict, Any
+from base64 import b64decode
+
+from algosdk import account, transaction
+from algosdk.v2client import algod
+
+# setup algod client
+algod_address = "https://testnet-api.algonode.cloud"
+algod_token = ""
+
+algod_client = algod.AlgodClient(algod_token, algod_address)
+
+# generate 2 random accounts, each with a fresh private key and associated account address
+account_1_private_key, account_1_address = account.generate_account()
+account_2_private_key, account_2_address = account.generate_account()
+account_3_private_key, account_3_address = account.generate_account()
+
+# time.sleep(int(10))
+
+print(f"Account 1 Base64 encoded private key: {account_1_private_key}")
+print(f"Account 1 Address: {account_1_address}")
+
+print(f"Account 2 Base64 encoded private key: {account_2_private_key}")
+print(f"Account 2 Address: {account_2_address}")
+
+print(f"Account 3 Base64 encoded private key: {account_3_private_key}")
+print(f"Account 3 Address: {account_3_address}")
+
+user_input = input(
+    "Now go to Algorand testnet dispenser (https://bank.testnet.algorand.network) and fund account 1 then return and press enter. "
+)
+
+
+# example: MULTISIG_CREATE
+version = 1  # multisig version
+threshold = 2  # how many signatures are necessary
+# create a Multisig given the set of participants and threshold
+msig = transaction.Multisig(
+    version,
+    threshold,
+    [account_1_address, account_2_address, account_3_address],
+)
+print("Multisig Address: ", msig.address())
+# example: MULTISIG_CREATE
+
+sp = algod_client.suggested_params()
+ptxn = transaction.PaymentTxn(account_1_address, sp, msig.address(), int(1e5)).sign(
+    account_1_private_key
+)
+txid = algod_client.send_transaction(ptxn)
+transaction.wait_for_confirmation(algod_client, txid, 4)
+# dont check response, assume it worked
+
+# example: MULTISIG_SIGN
+msig_pay = transaction.PaymentTxn(
+    msig.address(),
+    sp,
+    account_1_address,
+    0,
+    close_remainder_to=account_1_address,
+)
+msig_txn = transaction.MultisigTransaction(msig_pay, msig)
+msig_txn.sign(account_2_private_key)
+msig_txn.sign(account_3_private_key)
+txid = algod_client.send_transaction(msig_txn)
+result = transaction.wait_for_confirmation(algod_client, txid, 4)
+print(f"Payment made from msig account confirmed in round {result['confirmed-round']}")
+# example: MULTISIG_SIGN
+
+
+# example: ACCOUNT_REKEY
+# Any kind of transaction can contain a rekey
+rekey_txn = transaction.PaymentTxn(
+    account_1_address, sp, account_1_address, 0, rekey_to=account_2_address
+)
+signed_rekey = rekey_txn.sign(account_1_private_key)
+txid = algod_client.send_transaction(signed_rekey)
+result = transaction.wait_for_confirmation(algod_client, txid, 4)
+print(f"rekey transaction confirmed in round {result['confirmed-round']}")
+
+# Now we should get an error if we try to submit a transaction
+# signed with account_1s private key
+expect_err_txn = transaction.PaymentTxn(account_1_address, sp, account_1_address, 0)
+signed_expect_err_txn = expect_err_txn.sign(account_1_private_key)
+try:
+    txid = algod_client.send_transaction(signed_expect_err_txn)
+except Exception as e:
+    print("Expected error: ", e)
+
+# But its fine if we sign it with the account we rekeyed to
+signed_expect_err_txn = expect_err_txn.sign(account_2_private_key)
+txid = algod_client.send_transaction(signed_expect_err_txn)
+result = transaction.wait_for_confirmation(algod_client, txid, 4)
+print(f"transaction confirmed in round {result['confirmed-round']}")
+
+# rekey account1 back to itself so we can actually use it later
+rekey_txn = transaction.PaymentTxn(
+    account_1_address, sp, account_1_address, 0, rekey_to=account_1_address
+)
+signed_rekey = rekey_txn.sign(account_2_private_key)
+txid = algod_client.send_transaction(signed_rekey)
+result = transaction.wait_for_confirmation(algod_client, txid, 4)
+print(f"rekey transaction confirmed in round {result['confirmed-round']}")
+# example: ACCOUNT_REKEY
