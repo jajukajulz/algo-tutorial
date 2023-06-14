8353bc20be23d337d6df4365f324f5435fd0cbe4
Julian Kanjere <julian.kanjere@jskdezign.com>
2023-06-14 23:52:32 +0300

    add user input pauses and further examples

--- 6_algo_indexer.py
+++ 6_algo_indexer.py
@@ -9,6 +9,7 @@ import datetime
 try:
     my_address = "REPLACE_ME"
     txid = "REPLACE_ME"
+    asset_id = "REPLACE_ME"
 
     # Create a new indexer client, configured to connect to a node e.g. https://testnet-algorand.api.purestake.io/idx2
     # or https://testnet-idx.algonode.cloud
@@ -17,6 +18,9 @@ try:
     headers = ""
     indexer_client = indexer.IndexerClient(indexer_token, indexer_address, headers)
 
+    user_input = input("Press enter to proceed to indexer search for transactions by ID example...")
+
+    # example: INDEXER_SEARCH_TRANSACTIONS_BY_ID
     # Read the transaction by ID
     try:
         txn1 = indexer_client.transaction(txid)
@@ -27,8 +31,11 @@ try:
         # indexer search by address search_transactions_by_address
     except Exception as err:
         print(err)
+    # example: INDEXER_SEARCH_TRANSACTIONS_BY_ID
 
+    user_input = input("Press enter to proceed to indexer search for transactions on address example...")
 
+    # example: INDEXER_SEARCH_TRANSACTIONS_FOR_ADDRESS
     # Read the transactions by address
     try:
         txn_list = indexer_client.search_transactions_by_address(my_address)
@@ -43,6 +50,56 @@ try:
             print("Transaction not found")
     except Exception as err:
         print(err)
+    # example: INDEXER_SEARCH_TRANSACTIONS_FOR_ADDRESS
+
+    user_input = input("Press enter to proceed to indexer asset lookup example...")
+
+    # example: INDEXER_LOOKUP_ASSET
+    # lookup a single asset, by passing include_all, we specify that we want to see deleted assets as well
+    response = indexer_client.asset_info(asset_id, include_all=True)
+    print(f"Asset Info: {json.dumps(response, indent=2, )}")
+    # example: INDEXER_LOOKUP_ASSET
+
+    user_input = input("Press enter to proceed to indexer transaction search filtered by minimum amount example...")
+
+    # example: INDEXER_SEARCH_MIN_AMOUNT
+    response = indexer_client.search_transactions(
+        min_amount=10, min_round=1000, max_round=1500, address=my_address
+    )
+    print(f"Transaction results: {json.dumps(response, indent=2)}")
+    # example: INDEXER_SEARCH_MIN_AMOUNT
+
+    user_input = input("Press enter to proceed to indexer pagination example...")
+
+    # example: INDEXER_PAGINATE_RESULTS
+
+    nexttoken = ""
+    has_results = True
+    page = 0
+
+    # loop using next_page to paginate until there are
+    # no more transactions in the response
+    while has_results:
+        response = indexer_client.search_transactions(
+            min_amount=10, min_round=1000, max_round=1500, next_page=nexttoken, address=my_address
+        )
+
+        has_results = len(response["transactions"]) > 0
+
+        if has_results:
+            nexttoken = response["next-token"]
+            print(f"Tranastion on page {page}: " + json.dumps(response, indent=2))
+
+        page += 1
+    # example: INDEXER_PAGINATE_RESULTS
+
+    user_input = input("Press enter to proceed to indexer prefix search...")
+
+    # example: INDEXER_PREFIX_SEARCH
+    note_prefix = "showing prefix".encode()
+    response = indexer_client.search_transactions(note_prefix=note_prefix)
+    print(f"result: {json.dumps(response, indent=2)}")
+    # example: INDEXER_PREFIX_SEARCH
 
 except Exception as e:
     print(e)
