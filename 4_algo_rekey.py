b5d45a1257184099f59b44e7c95da85917ee3c1a
Julian Kanjere <julian.kanjere@jskdezign.com>
2023-06-15 14:32:55 +0300

    use existing accounts and pause after each transaction

--- 4_algo_rekey.py
+++ 4_algo_rekey.py
@@ -7,7 +7,7 @@ import json, time
 from typing import Dict, Any
 from base64 import b64decode
 
-from algosdk import account, transaction
+from algosdk import account, transaction, mnemonic
 from algosdk.v2client import algod
 
 # setup algod client
@@ -15,35 +15,37 @@ algod_address = "https://testnet-api.algonode.cloud"
 algod_token = ""
 
 algod_client = algod.AlgodClient(algod_token, algod_address)
+sp = algod_client.suggested_params()
 
-# generate 3 random accounts, each with a fresh private key and associated account address
-account_1_private_key, account_1_address = account.generate_account()
-account_2_private_key, account_2_address = account.generate_account()
-account_3_private_key, account_3_address = account.generate_account()
-
-# time.sleep(int(10))
+# example: ACCOUNT_RECOVER_MNEMONIC
+account_1_mnemonic = "REPLACE_ME"
+account_1_private_key = mnemonic.to_private_key(account_1_mnemonic)
+print(f"Base64 encoded private key: {account_1_private_key}")
+account_1_address = account.address_from_private_key(account_1_private_key)
 
 print(f"Account 1 Base64 encoded private key: {account_1_private_key}")
 print(f"Account 1 Address: {account_1_address}")
 
+account_2_mnemonic = "REPLACE_ME"
+account_2_private_key = mnemonic.to_private_key(account_2_mnemonic)
+print(f"Base64 encoded private key: {account_2_private_key}")
+account_2_address = account.address_from_private_key(account_2_private_key)
+
 print(f"Account 2 Base64 encoded private key: {account_2_private_key}")
 print(f"Account 2 Address: {account_2_address}")
 
+account_3_mnemonic = "REPLACE_ME"
+account_3_private_key = mnemonic.to_private_key(account_3_mnemonic)
+print(f"Base64 encoded private key: {account_3_private_key}")
+account_3_address = account.address_from_private_key(account_3_private_key)
+
 print(f"Account 3 Base64 encoded private key: {account_3_private_key}")
 print(f"Account 3 Address: {account_3_address}")
+# example: ACCOUNT_RECOVER_MNEMONIC
 
 user_input = input(
     "Now go to Algorand testnet dispenser (https://bank.testnet.algorand.network) and fund account 1 then return and press enter. "
 )
-
-sp = algod_client.suggested_params()
-ptxn = transaction.PaymentTxn(account_1_address, sp, msig.address(), int(1e5)).sign(
-    account_1_private_key
-)
-txid = algod_client.send_transaction(ptxn)
-transaction.wait_for_confirmation(algod_client, txid, 4)
-# dont check response, assume it worked
-
 
 # example: ACCOUNT_REKEY
 # Any kind of transaction can contain a rekey
@@ -55,6 +57,11 @@ txid = algod_client.send_transaction(signed_rekey)
 result = transaction.wait_for_confirmation(algod_client, txid, 4)
 print(f"rekey transaction confirmed in round {result['confirmed-round']}")
 
+user_input = input(
+    "Now we should get an error if we try to submit a transaction from account 1 \
+    and signed with account_1s private key. Press enter to continue... "
+)
+
 # Now we should get an error if we try to submit a transaction
 # signed with account_1s private key
 expect_err_txn = transaction.PaymentTxn(account_1_address, sp, account_1_address, 0)
@@ -64,11 +71,21 @@ try:
 except Exception as e:
     print("Expected error: ", e)
 
+user_input = input(
+    "Now we should NOT get an error if we try to submit a transaction from account 1 \
+    and sign with account_2s private key. Press enter to continue... "
+)
+
 # But its fine if we sign it with the account we rekeyed to
 signed_expect_err_txn = expect_err_txn.sign(account_2_private_key)
 txid = algod_client.send_transaction(signed_expect_err_txn)
 result = transaction.wait_for_confirmation(algod_client, txid, 4)
 print(f"transaction confirmed in round {result['confirmed-round']}")
+
+user_input = input(
+    "Now we rekey account 1 back to using its own private key i.e. account 1 private key.\
+          Press enter to continue... "
+)
 
 # rekey account1 back to itself so we can actually use it later
 rekey_txn = transaction.PaymentTxn(
